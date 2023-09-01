0a5095a16a51c2d6dee356be52184f5c1087962d
Jay Simons <jay@designly.biz>
2023-08-31 19:51:23 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-4xl font-bold">Next.js 13</h1>
+    </div>
+  )
+}
